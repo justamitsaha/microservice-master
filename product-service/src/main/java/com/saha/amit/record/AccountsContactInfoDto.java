6c5ee387613ce679a987066ab83bfb6b29ae0aef
justamitsaha <justamitsaha@gmail.com>
2024-06-14 18:35:06 +0530

    Config Server

--- product-service/src/main/java/com/saha/amit/record/AccountsContactInfoDto.java
+++ product-service/src/main/java/com/saha/amit/record/AccountsContactInfoDto.java
@@ -6,5 +6,33 @@ import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+public class AccountsContactInfoDto{
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupportString;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Map<String, String> getContactDetails() {
+        return contactDetails;
+    }
+
+    public void setContactDetails(Map<String, String> contactDetails) {
+        this.contactDetails = contactDetails;
+    }
+
+    public List<String> getOnCallSupportString() {
+        return onCallSupportString;
+    }
+
+    public void setOnCallSupportString(List<String> onCallSupportString) {
+        this.onCallSupportString = onCallSupportString;
+    }
 }
