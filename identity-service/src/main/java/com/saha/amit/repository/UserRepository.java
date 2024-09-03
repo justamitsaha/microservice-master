81193a60c24f5f5b3f3b29c68dc20a7777b8ae4f
justamitsaha <justamitsaha@gmail.com>
2024-09-03 07:22:54 +0530

    Join query

--- identity-service/src/main/java/com/saha/amit/repository/UserRepository.java
+++ identity-service/src/main/java/com/saha/amit/repository/UserRepository.java
@@ -15,9 +15,15 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     public List<User> findByEmailContaining(String email);
 
-    @Query("SELECT u FROM User u JOIN u.userPreference up WHERE up.okToMail = :okToMail")
-    List<User> findUsersByMailPreference(@Param("okToMail") boolean okToMail);
+    @Query("SELECT u FROM User u JOIN u.userPreferenceReference up WHERE up.okToMail = true")
+    List<User> findUsersWithMailPreference();
 
-    @Query(value = "SELECT u.* FROM user u JOIN user_preference up ON u.id = up.user_id WHERE up.ok_to_mail = :okToMail", nativeQuery = true)
-    List<User> findUsersByMailPreferenceNative(@Param("okToMail") boolean okToMail);
+//    @Query("SELECT u FROM User u JOIN u.userPreference up WHERE up.okToMail = :okToMail")
+//    List<User> findUsersByMailPreference(@Param("okToMail") boolean okToMail);
+//
+//    @Query(value = "SELECT u.* FROM user u JOIN user_preference up ON u.id = up.user_id WHERE up.ok_to_mail = :okToMail", nativeQuery = true)
+//    List<User> findUsersByMailPreferenceNative(@Param("okToMail") boolean okToMail);
+
+    @Query(value = "SELECT u.* FROM user_table u JOIN user_preference up ON u.id = up.user_id WHERE up.ok_to_mail = true", nativeQuery = true)
+    List<User> findUsersWithMailPreferenceNative();
 }
