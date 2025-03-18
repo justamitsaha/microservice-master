fd762e2362fde21a9f5baf2d9c263bd70b5941aa
justamitsaha <justamitsaha@gmail.com>
2025-03-18 22:27:41 +0530

    Cleanup

--- util-service/src/main/java/com/saha/amit/dto/ProductDto.java
+++
@@ -1,4 +0,0 @@
-package com.saha.amit.dto;
-
-public record ProductDto(int productId, String productName, String productDescription, int price, String imagePath, String category, int userId,  String userName) {
-}
